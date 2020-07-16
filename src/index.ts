e189aacbbb578369d61c2e4f1547a5f3558fc430
Callum Macrae <callum@macr.ae>
2020-07-16 17:38:41 +0100

    Move intro text out of index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import World from './world';
 import Level from './level';
 import { InstructionFnType } from './character';
 
+import introText from './utils/intro-text';
+
 const app = new PIXI.Application({
   backgroundColor: 0x211f27,
 });
@@ -27,48 +29,6 @@ app.loader.load(() => {
   app.stage.addChild(worldContainer);
 
   function showIntro() {
-    const introText = [
-      [
-        'hello!',
-        '',
-        'in this game, you code a robot to',
-        'outrun progressively smarter enemies.',
-        '',
-        "it's playable in codepen: your javascript",
-        'tells the robot where to run.',
-        '',
-        'click to continue.',
-      ].join('\n'),
-      [
-        'right now, the robot is moving around',
-        'randomly, and so is getting caught often.',
-        '',
-        "don't worry though - when the game",
-        'starts there will only be one enemy',
-        'and it will be much easier to run from.',
-        '',
-        'click to continue.',
-      ].join('\n'),
-      [
-        'as you progress through the game,',
-        'the algorithms powering the enemies',
-        'get progressively cleverer.',
-        '',
-        'if you want to beat them, your code will',
-        'have to be smarter!',
-        '',
-        'click to continue.',
-      ].join('\n'),
-      [
-        'take a look at the javascript pane.',
-        '',
-        'there are two functions: setup controls',
-        'the game while instruction controls the',
-        'robot.',
-        '',
-        'uncomment out the play() call to begin.',
-      ].join('\n'),
-    ];
     let currentIndex = 0;
     world.setOverlay(introText[0]);
 
---
+++ src/utils/intro-text.ts
@@ -0,0 +1,44 @@
+const introText = [
+  [
+    'hello!',
+    '',
+    'in this game, you code a robot to',
+    'outrun progressively smarter enemies.',
+    '',
+    "it's playable in codepen: your javascript",
+    'tells the robot where to run.',
+    '',
+    'click to continue.',
+  ].join('\n'),
+  [
+    'right now, the robot is moving around',
+    'randomly, and so is getting caught often.',
+    '',
+    "don't worry though - when the game",
+    'starts there will only be one enemy',
+    'and it will be much easier to run from.',
+    '',
+    'click to continue.',
+  ].join('\n'),
+  [
+    'as you progress through the game,',
+    'the algorithms powering the enemies',
+    'get progressively cleverer.',
+    '',
+    'if you want to beat them, your code will',
+    'have to be smarter!',
+    '',
+    'click to continue.',
+  ].join('\n'),
+  [
+    'take a look at the javascript pane.',
+    '',
+    'there are two functions: setup controls',
+    'the game while instruction controls the',
+    'robot.',
+    '',
+    'uncomment out the play() call to begin.',
+  ].join('\n'),
+];
+
+export default introText;
